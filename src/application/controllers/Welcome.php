92338d80e37942df61c7407595dec0d823ef5dc5
josergm86 <josergm86@gmail.com>
2019-02-17 19:32:56 -0300

    a

--- src/application/controllers/Welcome.php
+++ src/application/controllers/Welcome.php
@@ -59,8 +59,7 @@ class Welcome extends CI_Controller {
         } catch (Exception $exc) {
             header("Location:" . $GLOBALS['sistem_config']->BASE_SITE_URL);
             echo $exc->getMessage();
-        }
-        header("Location:" . $GLOBALS['sistem_config']->BASE_SITE_URL);
+        }        
     }
     
 
